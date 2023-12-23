7658ab70b7a70cd7644cb40b4caed76215411377
tink <qietingfy@gmail.com>
2023-12-23 21:36:31 +0800

    env_logger init

--- actix-web-helloworld/src/main.rs
+++ actix-web-helloworld/src/main.rs
@@ -1,5 +1,8 @@
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
     let app = actix_web_helloworld::App::new(8000);
     app.run().await
 }
